7a436410238dd8fb82085242da4edc071628e62e
popomore <sakura9515@gmail.com>
2013-12-03 23:15:13 +0800

    update

--- dist/util/template-debug.js
+++ dist/util/template-debug.js
@@ -2,7 +2,7 @@
  * User: linweilong(TGL)
  * Date: 2013-12-03 22:52
  */
-define("util/template-debug", [ "../util-debug", "./date-format-debug", "./-debug" ], function(require) {
+define("util/template-debug", [ "../util-debug", "./date-format-debug" ], function(require) {
     var util = require("../util-debug");
     var dformat = require("./date-format-debug");
     return {};
--- dist/util/template.js
+++ dist/util/template.js
@@ -2,7 +2,7 @@
  * User: linweilong(TGL)
  * Date: 2013-12-03 22:52
  */
-define("util/template", [ "../util", "./date-format", "./" ], function(require) {
+define("util/template", [ "../util", "./date-format" ], function(require) {
     var util = require("../util");
     var dformat = require("./date-format");
     return {};
